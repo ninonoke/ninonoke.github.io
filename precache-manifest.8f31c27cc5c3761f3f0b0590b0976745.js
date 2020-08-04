3f581406a95cb07dd198afb44372943c86c03312
ninonoke <nino.gelovani@ied.edu>
2020-08-04 19:50:58 +0200

    updated build

--- precache-manifest.8f31c27cc5c3761f3f0b0590b0976745.js
+++ precache-manifest.8f31c27cc5c3761f3f0b0590b0976745.js
@@ -5,50 +5,50 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "d479e3d733d22ccd13cf",
-    "url": "/static/css/main.4438f93b.chunk.css"
+    "url": "static/css/main.4438f93b.chunk.css"
   },
   {
     "revision": "47ed0d52231b7ab440ff",
-    "url": "/static/js/2.6648418b.chunk.js"
+    "url": "static/js/2.6648418b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.6648418b.chunk.js.LICENSE.txt"
+    "url": "static/js/2.6648418b.chunk.js.LICENSE.txt"
   },
   {
     "revision": "d479e3d733d22ccd13cf",
-    "url": "/static/js/main.ca1e35fd.chunk.js"
+    "url": "static/js/main.ca1e35fd.chunk.js"
   },
   {
     "revision": "d7aad67ac875fe34d07a",
-    "url": "/static/js/runtime-main.bc4ca61f.js"
+    "url": "static/js/runtime-main.bc4ca61f.js"
   },
   {
     "revision": "109d73370ca0142668d1e6684aeb8bbb",
-    "url": "/static/media/Background.109d7337.png"
+    "url": "static/media/Background.109d7337.png"
   },
   {
     "revision": "12fbd530d4e189af9e722808396c8ea6",
-    "url": "/static/media/Media.12fbd530.png"
+    "url": "static/media/Media.12fbd530.png"
   },
   {
     "revision": "f3e435d820367cb639af7c15c72ed66b",
-    "url": "/static/media/MediaBackground.f3e435d8.png"
+    "url": "static/media/MediaBackground.f3e435d8.png"
   },
   {
     "revision": "ab8244010c24b55dc7d6dbcc40a6f877",
-    "url": "/static/media/Rectangle.ab824401.png"
+    "url": "static/media/Rectangle.ab824401.png"
   },
   {
     "revision": "e056362ce1564043fd05fe47f7587fde",
-    "url": "/static/media/Rectangle1.e056362c.png"
+    "url": "static/media/Rectangle1.e056362c.png"
   },
   {
     "revision": "e201b3ed4deabccc2f401428f538eee6",
-    "url": "/static/media/Rectangle2.e201b3ed.png"
+    "url": "static/media/Rectangle2.e201b3ed.png"
   },
   {
     "revision": "a7f6435d022702a40936a92c40201233",
-    "url": "/static/media/Rectangle3.a7f6435d.png"
+    "url": "static/media/Rectangle3.a7f6435d.png"
   }
 ]);
