0b8185bcaf41d1a2feadb7194463a3583cd83a8d
ninonoke <nino.gelovani@ied.edu>
2020-08-04 19:05:57 +0200

    add build files

---
+++ precache-manifest.8f31c27cc5c3761f3f0b0590b0976745.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437d50a3be3d6cb1692b31663ad70442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d479e3d733d22ccd13cf",
+    "url": "/static/css/main.4438f93b.chunk.css"
+  },
+  {
+    "revision": "47ed0d52231b7ab440ff",
+    "url": "/static/js/2.6648418b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6648418b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d479e3d733d22ccd13cf",
+    "url": "/static/js/main.ca1e35fd.chunk.js"
+  },
+  {
+    "revision": "d7aad67ac875fe34d07a",
+    "url": "/static/js/runtime-main.bc4ca61f.js"
+  },
+  {
+    "revision": "109d73370ca0142668d1e6684aeb8bbb",
+    "url": "/static/media/Background.109d7337.png"
+  },
+  {
+    "revision": "12fbd530d4e189af9e722808396c8ea6",
+    "url": "/static/media/Media.12fbd530.png"
+  },
+  {
+    "revision": "f3e435d820367cb639af7c15c72ed66b",
+    "url": "/static/media/MediaBackground.f3e435d8.png"
+  },
+  {
+    "revision": "ab8244010c24b55dc7d6dbcc40a6f877",
+    "url": "/static/media/Rectangle.ab824401.png"
+  },
+  {
+    "revision": "e056362ce1564043fd05fe47f7587fde",
+    "url": "/static/media/Rectangle1.e056362c.png"
+  },
+  {
+    "revision": "e201b3ed4deabccc2f401428f538eee6",
+    "url": "/static/media/Rectangle2.e201b3ed.png"
+  },
+  {
+    "revision": "a7f6435d022702a40936a92c40201233",
+    "url": "/static/media/Rectangle3.a7f6435d.png"
+  }
+]);
